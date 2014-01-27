ea83c58ede4d449012240879c6d2c1a9700dbca5
anjasi <anja-sinz@web.de>
2014-01-27 19:21:15 +0100

    Update konstanten.rb

--- konstanten.rb
+++ konstanten.rb
@@ -6,7 +6,26 @@ ZELLEN_GROESSE = 10
 ZELLEN_ANZAHL = 48 # 50 Zellen abzüglich 2 Zellen für den Rand 
 ECKE_SPIELFELD_X = 100
 ECKE_SPIELFELD_Y = 150
-#OFFSET = 40			# Entfernung Rand->Spielfeld, ge�ndert von 50 auf 40 am 24.01
-#X_START_SCHLANGE =
-#Y_START_SCHLANGE = 
 #GESCHWINDIGKEIT = 5
+
+# In dieser Klasse werden die Default-Werte für die einzelnen Zellen festgelegt
+class Status
+  HINTERGRUND = 0
+  ESSEN = 1 
+  SCHLANGE = 2
+end
+
+# Farben der jeweiligen Zellen
+FARBEN = {
+  Status::HINTERGRUND => "grey",      # Farbe in doppelte Hochkommata??
+  Status::ESSEN => "red",
+  Status::SCHLANGE => "green"
+  }
+  
+# Richtungen
+RICHTUNG = {
+  :up => [0,-1],
+  :down => [0,1],
+  :left => [-1,0],
+  :right => [1,0]
+  }
